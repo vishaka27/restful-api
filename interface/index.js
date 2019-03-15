62d3c738aa9a54939d81aa66359c43fe12eaf754
vish27 <vishaka@zestmoney.in>
2019-03-15 12:28:57 +0530

    integrated get request

--- interface/index.js
+++ interface/index.js
@@ -1,19 +1,66 @@
-const url = 'http://localhost:5001/addUser';
+const http = new XMLHttpRequest();
+const posturl = 'http://localhost:5001/addUser';
+const response = new Response();
+var data;
 
 function addUser() {
-
-    const user = {
-        'user4': {
-            'name': 'mark',
-            'password': 'bufallo',
-            'profession': 'software professional',
-            'id': 4
+    var name = document.getElementById('name').value;
+    var password = document.getElementById('password').value;
+    var profession = document.getElementById('profession').value;
+    axios.get('http://localhost:5001/listUsers').then(response => {
+        users = response.data;
+        count = Object.keys(users).length;
+        const user = {
+            'user4': {
+                'name': name,
+                'password': password,
+                'profession': profession,
+                'id': count + 1
+            }
         }
-    }
+        fetch(posturl, {
+            method: 'POST',
+            headers: {'Content-Type':'application/x-www-form-urlencoded'},
+            body: JSON.stringify(user.user4)
+        });
+    });
+}
 
-    fetch(url, {
-        method: 'POST',
-        headers: {'Content-Type':'application/x-www-form-urlencoded'},
-        body: JSON.stringify(user.user4)
+function showDetails() {
+    const id = document.getElementById("field").value;
+    axios.get('http://localhost:5001/' + id).then(response => {
+        data = response.data;
+        if (data) {
+            // CREATE DYNAMIC TABLE.
+            var table = document.createElement("table");
+
+            // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+            var thead = table.createTHead();                   // TABLE ROW.
+            var col = ["S.No", "Name", "Password", "Profession"];
+            for (var i = 0; i < col.length; i++) {
+                var th = document.createElement("th");      // TABLE HEADER.
+                th.innerHTML = col[i];
+                thead.appendChild(th);
+            }
+            tr = table.insertRow(-1);
+            // ADD JSON DATA TO THE TABLE AS ROWS.
+            for (var i = 0; i < Object.keys(data).length; i++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = data[`${Object.keys(data)[i]}`];
+            }
+
+            // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+            var divContainer = document.getElementById("showData");
+            divContainer.innerHTML = "";
+            divContainer.appendChild(table);
+            document.getElementById('noData').style.display = 'none';
+        }
+        if (data === '') {
+            var divContainer = document.getElementById('noData');
+            divContainer.innerHTML = 'No records found';
+            divContainer.style.display = 'block';
+            divContainer.style.color = 'red';
+        }
     });
 }
--- node/server.js
+++ node/server.js
@@ -7,7 +7,19 @@ global.appRoot = path.resolve(__dirname);
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+  
+
 const port = process.env.port || 5001;
+
 
 app.get('/', (req, res) => {
     res.send('Hello');
@@ -26,13 +38,22 @@ app.use(parser.json({ type: 'application/*+json' }));
 app.post('/addUser', function(req, res) {
     fs.readFile(__dirname + '/' + 'users.json', 'utf8', function(err, data) {
         data = JSON.parse(data);
-        data['user4'] = JSON.parse(Object.keys(req.body)[0]);
+        count = Object.keys(data).length;
+        data["user" + (count + 1)] = JSON.parse(Object.keys(req.body)[0]);
         json = JSON.stringify(data); //convert it back to json
         fs.writeFile('users.json',JSON.stringify(data), 'utf-8', function(err){
             if(err) throw err;
             res.end(JSON.stringify(data));
         }); 
     });
+});
+
+app.get('/:id', function(req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        var users = JSON.parse(data);
+        var user = users["user" + req.params.id];
+        res.send(user);
+     });
 });
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
